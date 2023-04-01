8dac0ab22d1fe2a690cee0935536812e99684d15
halki117 <s.halki0203@icloud.com>
2023-04-01 16:31:59 +0900

    ログイン画面、ユーザー登録画面：viewファイル作成、及びrouteの設定

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const express = require("express");
 const app = express();
 
+// ejsファイルをexpressに読み込む
+// 第一引数：view engineを指定。 第二引数：使用するテンプレートを指定。
+app.set('view engine','ejs');
+
+//ログイン画面
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//ユーザー登録画面
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(8080, ()=>{});
