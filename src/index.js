ef3c7806548c2b2da28b290b370052f4a0f97558
halki117 <s.halki0203@icloud.com>
2023-04-01 18:06:19 +0900

    view,route 実装

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 const express = require("express");
 const app = express();
 
+// ejsファイルをexpressに読み込む
+// 第一引数：view engineを指定。 第二引数：使用するテンプレートを指定。
+app.set('view engine','ejs');
+
+// cssを反映させるため
+app.use(express.static('public'))
+
+//ログイン画面
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+//ユーザー登録画面
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
 app.listen(8080, ()=>{});
