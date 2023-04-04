38dc2ff4c27765f9d170b96c449ba02838007cc6
halki117 <s.halki0203@icloud.com>
2023-04-04 23:21:05 +0900

    nodemon install

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
 
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: ''
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log('Connected');
+});
+
+
 // ejsファイルをexpressに読み込む
 // 第一引数：view engineを指定。 第二引数：使用するテンプレートを指定。
 app.set('view engine','ejs');
