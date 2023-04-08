be255f413b1eedfe8b07cf55cd85bd226bfc1125
halki117 <s.halki0203@icloud.com>
2023-04-08 16:51:50 +0900

    userテーブルへのレコード挿入

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -1,6 +1,22 @@
 "use strict";
+const bcrypt = require('bcrypt');
+const User = require('../../models').User;
 exports.signup = (req, res, next) => {
-    res.send('signup');
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.pass;
+    bcrypt.hash(password, 10).then((hashed_password) => {
+        User.create({
+            name: name,
+            email: email,
+            password: hashed_password,
+        }).then((user) => {
+            console.log(user);
+        });
+    });
+    // User.findAll().then(users => {
+    //   res.send(users);
+    // });
 };
 // exports.index = (req: any, res: any, next: any) => {
 //   res.send('index');
--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -1,5 +1,28 @@
+const bcrypt = require('bcrypt');
+const User = require('../../models').User;
+
 exports.signup = (req: any, res: any, next: any) => {
-  res.send('signup');
+
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.pass;
+
+  bcrypt.hash(password, 10).then((hashed_password: string) => {
+    User.create({
+      name: name,
+      email: email,
+      password: hashed_password,
+    }).then((user: any) => {
+      console.log(user);
+    });
+  });
+
+
+  // User.findAll().then(users => {
+
+  //   res.send(users);
+
+  // });
 };
 
 // exports.index = (req: any, res: any, next: any) => {
