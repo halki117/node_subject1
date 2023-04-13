96ebd09643db8270a0d512c17f38a4992844c07f
halki117 <s.halki0203@icloud.com>
2023-04-13 21:29:25 +0900

    tsファイルコンパイル時のエラー対応
    cannot find type definition file for 'ms'. the file is in the program because:
    
    と表示されていたので以下コマンド実行
    
    npm install @types/node --save-dev
    
    その後、npx tsc --watch で正常にコンパイル

---
+++ src/controllers/sessionController.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.login = (req, res, next) => {
+    const session = require('express-session');
+    const bcrypt = require('bcrypt');
+    const User = require('../../models').User;
+    const name = req.body.name;
+    const email = req.body.email;
+    const pass = req.body.pass;
+    User.findOne({
+        where: {
+            name: name,
+        }
+    }).then((user) => {
+        bcrypt.compare(pass, user.password).then((res) => {
+            console.log(res); // return true
+            req.session.name = user.name;
+            req.session.email = user.email;
+            req.session.save();
+        })
+            .catch((err) => console.error(err.message));
+        res.redirect('/');
+    });
+};
+exports.logout = (req, res, next) => {
+    req.session.destroy();
+    res.redirect('/');
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,61 @@
+"use strict";
+const registerController = require("./controllers/registerController");
+const sessionController = require("./controllers/sessionController");
 const express = require("express");
 const app = express();
-
+app.set("trust proxy", 1);
+const session = require('express-session');
+// 初期設定　sessionの設定
+const ses_opt = {
+    secret: 'my_secret',
+    // 画面遷移した際にセッションの保存がされなかったので以下の設定を一旦コメントアウト
+    // resave: false,          //trueにするとsessionに変更がなくても強制的に保存　通常false
+    // saveUninitialized: false, //trueにすると初期はされていなくても保存 通常false
+    cookie: {
+        maxAge: 1000 * 60 * 30, //cookieの寿命　単位はミリ秒
+    }
+};
+app.use(session(ses_opt));
+// postの値を受け取るために以下を追記(body-parserインストール済)
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// バリデーション
+const registerValidator = require('./validators/registerValidator');
+// DBへの接続
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: ''
+});
+con.connect(function (err) {
+    if (err)
+        throw err;
+    console.log('Connected');
+});
 // ejsファイルをexpressに読み込む
 // 第一引数：view engineを指定。 第二引数：使用するテンプレートを指定。
-app.set('view engine','ejs');
-
+app.set('view engine', 'ejs');
 // cssを反映させるため
-app.use(express.static('public'))
-
+app.use(express.static('public'));
+//ルーティング
+//ホーム画面
+app.get("/", (req, res, next) => {
+    console.log(req.session.name);
+    res.render("home", { login_user: req.session.name });
+});
 //ログイン画面
 app.get("/login", (req, res) => {
-  res.render("login");
+    res.render("login");
 });
-
 //ユーザー登録画面
 app.get("/register", (req, res) => {
-  res.render("register");
+    res.render("register");
 });
-
-app.listen(8080, ()=>{});
+app.post("/signup", registerValidator, registerController.signup);
+// ログイン
+app.post("/login", sessionController.login);
+// ログアウト
+app.get("/logout", sessionController.logout);
+app.listen(8080, () => { });
